40807302cc9ea0c5837412090514562fc813f37d
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-01-15 10:40:02 -0500

    remove group-hover effect on PlaylistCard

--- app/playlist_test/page.tsx
+++ app/playlist_test/page.tsx
@@ -7,24 +7,22 @@ export default function PlaylistCard() {
   const user_name = 'Austin Wald';
 
   return (
-    <div className="group ease-in-out transition-all flex flex-col justify-center items-center max-w-fit rounded-xl p-6 gap-6 bg-gray hover:bg-gray-light hover:bg-opacity-20 hover:shadow-xl">
+    <div className="flex flex-col justify-center items-center max-w-fit rounded-xl p-6 gap-6 bg-gray-light bg-opacity-20 shadow-xl">
       <div className="flex flex-row gap-6 justify-evenly items-center w-full">
         <Image
           src={playlist_image_url}
           width={150}
           height={150}
           alt="Playlist Image"
-          className="rounded-xl brightness-50 group-hover:brightness-100 group-hover:shadow-xl duration-300"
+          className="rounded-xl shadow-xl"
         />
         <div>
-          <h1 className="text-3xl font-semibold text-gray-light group-hover:text-white">
-            {playlist_name}
-          </h1>
+          <h1 className="text-3xl font-semibold text-white">{playlist_name}</h1>
           <h3 className="text-xl font-semibold text-gray-light">by {user_name}</h3>
         </div>
       </div>
-      <button className="rounded-full p-4 bg-gray group-hover:bg-green duration-300 text-gray-light group-hover:text-white text-l font-medium hover:shadow-xl hover:scale-105">
-        Remove Duplicates
+      <button className="rounded-full p-4 bg-green text-white text-l font-medium hover:shadow-xl hover:scale-105 duration-300">
+        Find Duplicates
       </button>
     </div>
   );
