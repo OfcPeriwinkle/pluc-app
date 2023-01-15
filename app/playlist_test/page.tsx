b48a5e3694f1853e2d9cf79cd319868ccb3d72ae
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-01-14 21:04:01 -0500

    add group hover styling

--- app/playlist_test/page.tsx
+++ app/playlist_test/page.tsx
@@ -7,21 +7,23 @@ export default function PlaylistCard() {
   const user_name = 'Austin Wald';
 
   return (
-    <div className="flex flex-col justify-center items-center max-w-fit bg-gray-light rounded-xl bg-opacity-20 p-6 gap-6 shadow-xl">
+    <div className="group ease-in-out transition-all flex flex-col justify-center items-center max-w-fit rounded-xl p-6 gap-6 bg-gray hover:bg-gray-light hover:bg-opacity-20 hover:shadow-xl">
       <div className="flex flex-row gap-6 justify-evenly items-center w-full">
         <Image
           src={playlist_image_url}
           width={150}
           height={150}
           alt="Playlist Image"
-          className="rounded-xl shadow-xl"
+          className="rounded-xl brightness-50 group-hover:brightness-100 group-hover:shadow-xl duration-300"
         />
         <div>
-          <h1 className="text-3xl text-white font-semibold">{playlist_name}</h1>
-          <h3 className="text-xl text-zinc-400 font-semibold">by {user_name}</h3>
+          <h1 className="text-3xl font-semibold text-gray-light group-hover:text-white">
+            {playlist_name}
+          </h1>
+          <h3 className="text-xl font-semibold text-gray-light">by {user_name}</h3>
         </div>
       </div>
-      <button className="rounded-full bg-green p-4 hover:scale-105 text-l font-medium shadow-xl hover:shadow-2xl">
+      <button className="rounded-full p-4 bg-gray group-hover:bg-green duration-300 text-gray-light group-hover:text-white text-l font-medium hover:shadow-xl hover:scale-105">
         Remove Duplicates
       </button>
     </div>
