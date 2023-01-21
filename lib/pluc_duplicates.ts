ad125d1a6093007597b4f65d9539a9b69451317e
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-01-20 21:24:06 -0500

    add return type to get_duplicates; re-organize for better seperation of concerns; add test json containing duplicate track results; remove outdated test json

--- lib/pluc_duplicates.ts
+++ lib/pluc_duplicates.ts
@@ -4,6 +4,33 @@ import 'lodash.combinations';
 import 'lodash.product';
 import _ from 'lodash';
 
+// TODO: have these be configurable by the user
+const TIME_DIFF_THRESHOLD_MS = 5000;
+const JARO_WINKLER_SIMILARITY_THRESHOLD = 0.7;
+
+/**
+ * DuplicateGraph
+ *
+ * Adjaceny lists for all tracks containing duplicates.
+ *
+ * Each key is a track ID that has duplicates and each value is a tuple containing metadata for the
+ * track whose ID is being used as the key and list of that track's identified duplicates.
+ */
+interface DuplicateGraph {
+  [track_id: string]: [Track, Track[]];
+}
+
+/**
+ * Dictionary of duplicate track results by artist.
+ *
+ * Each key is an artist ID for an artist who has duplicate tracks in the processed playlist.
+ * Each value is a tuple containing metadata for the artist whose ID is the entry's key and
+ * a DuplicateGraph of the duplicated tracks for that artist.
+ */
+interface DuplicateResults {
+  [artist_id: string]: [Artist, DuplicateGraph];
+}
+
 interface PlucAlbum extends SimplifiedAlbum {
   track_nodes: Track[];
 }
@@ -16,12 +43,14 @@ interface ArtistDict {
   [artist_id: string]: PlucArtist;
 }
 
-// TODO: have these be configurable by the user
-const TIME_DIFF_THRESHOLD_MS = 5000;
-const JARO_WINKLER_SIMILARITY_THRESHOLD = 0.7;
-
-export default function get_duplicates(playlist_tracks: PlaylistTrack[]) {
+export default function get_duplicates(playlist_tracks: PlaylistTrack[]): DuplicateResults {
   console.log('Getting duplicates...');
+  const artist_dict = build_pluc_tree(playlist_tracks);
+  return find_duplicates(artist_dict);
+  console.log('Done!');
+}
+
+function build_pluc_tree(playlist_tracks: PlaylistTrack[]): ArtistDict {
   const artist_dict: ArtistDict = {};
 
   playlist_tracks.map((entry) => {
@@ -59,8 +88,7 @@ export default function get_duplicates(playlist_tracks: PlaylistTrack[]) {
     album_node['track_nodes'].push(track);
   });
 
-  find_duplicates(artist_dict);
-  console.log('Done!');
+  return artist_dict;
 }
 
 /**
@@ -68,25 +96,27 @@ export default function get_duplicates(playlist_tracks: PlaylistTrack[]) {
  * This might make a ton of sense as a Python serveless function since Python
  * is so so so so so SO much better at math and statistics stuff
  *
- * TODO: make private and return something!
  */
-function find_duplicates(artist_dict: ArtistDict) {
+function find_duplicates(artist_dict: ArtistDict): DuplicateResults {
+  let duplicates_by_artist: DuplicateResults = {};
+
   Object.entries(artist_dict).map(([artist_id, artist_node]) => {
+    let duplicate_graph: DuplicateGraph = {};
     const albums = artist_node.album_nodes;
 
     if (Object.keys(albums).length === 1) {
       return;
     }
 
+    // Get all possible combinations of albums from those present in the playlist
     const permutations = _.combinations(Object.keys(albums), 2);
-
     permutations.map(([album_id_a, album_id_b]: string[]) => {
       const track_nodes_a = albums[album_id_a].track_nodes;
       const track_nodes_b = albums[album_id_b].track_nodes;
-      const track_combinations = _.product(track_nodes_a, track_nodes_b);
 
-      // Conduct comparison
-      track_combinations.map(([track_a, track_b]: Track[]) => {
+      // Get all possible ordered pairs of tracks between the two albums
+      const track_pairs = _.product(track_nodes_a, track_nodes_b);
+      track_pairs.map(([track_a, track_b]: Track[]) => {
         // TODO: Spotify allows songs with empty names; we should handle this eventually since these
         // tracks are unplayable and should be removed
         if (track_a.name.length === 0 || track_b.name.length === 0) {
@@ -108,22 +138,41 @@ function find_duplicates(artist_dict: ArtistDict) {
         // determination since string similarity only gets us so far
         // Calculate similarity metric
         const similarity = jaroWinkler(track_a.name, track_b.name, { caseSensitive: true });
-        if (similarity > JARO_WINKLER_SIMILARITY_THRESHOLD) {
-          console.log(
-            'Duplicate Detected!',
-            '\nA:',
-            track_a.name,
-            track_a.id,
-            '\nB:',
-            track_b.name,
-            track_b.id,
-            '\nSimilarity:',
-            similarity,
-            '\nTime Diff:',
-            time_diff
-          );
+        if (similarity < JARO_WINKLER_SIMILARITY_THRESHOLD) {
+          return;
         }
+
+        // Tracks are likely duplicates create a duplicate graph if the current track doesn't have
+        // one and add the duplicate track to the current node's adjacency list
+        if (!(track_a.id in duplicate_graph)) {
+          duplicate_graph[track_a.id] = [track_a, []];
+        }
+
+        duplicate_graph[track_a.id][1].push(track_b);
+        console.log(
+          'Duplicate Detected!',
+          '\nA:',
+          track_a.name,
+          track_a.id,
+          '\nB:',
+          track_b.name,
+          track_b.id,
+          '\nSimilarity:',
+          similarity,
+          '\nTime Diff:',
+          time_diff
+        );
       });
     });
+
+    if (artist_id in duplicates_by_artist) {
+      throw Error("Artist's duplicate graph is about to be overridden");
+    }
+
+    console.log(artist_node.name, duplicate_graph);
+    duplicates_by_artist[artist_id] = [artist_node, duplicate_graph];
   });
+
+  console.log(duplicates_by_artist);
+  return duplicates_by_artist;
 }
