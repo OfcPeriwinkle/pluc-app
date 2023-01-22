219012b3dd7d2a02da9159249174aef887156118
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-01-21 21:36:59 -0500

    fix navbar links; rename Header links prop to href; change anchor tag to next/link component for prefetch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,7 @@ import './globals.css';
 import Header from './Components/Header';
 import ProvidersWrapper from './Components/ProvidersWrapper';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       {/*
@@ -17,9 +13,10 @@ export default function RootLayout({
       <body className="bg-gradient-to-b from-gray-dark to-gray text-white tracking-tighter">
         <Header
           links={[
-            { label: 'pluc', link: '/' },
-            { label: 'coming soon', link: '/coming_soon' },
-            { label: 'about', link: '/about' },
+            { label: 'pluc', href: '/' },
+            { label: 'sandbox', href: '/sandbox' },
+            { label: 'coming soon', href: '/coming_soon' },
+            { label: 'about', href: '/about' },
           ]}
         />
 
