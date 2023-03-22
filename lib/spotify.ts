31966804b18fa9cac23451ec65e08d2882f4bfe3
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-03-22 19:54:44 -0400

    fix get_access_token; style pass

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,34 +1,32 @@
-import { PlaylistTrack } from "spotify-types";
+import { PlaylistTrack } from 'spotify-types';
 
-const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
-const SPOTIFY_API_ROOT = "https://api.spotify.com/v1";
+const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
+const SPOTIFY_API_ROOT = 'https://api.spotify.com/v1';
 
 const client_id = process.env.SPOTIFY_ID;
 const client_secret = process.env.SPOTIFY_SECRET;
-const basic_authorization = Buffer.from(
-  `${client_id}:${client_secret}`
-).toString("base64");
+const basic_authorization = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 
 /** Use a refresh_token to acquire a new access_token */
 export async function get_access_token(active_refresh_token: string) {
   const res = await fetch(TOKEN_ENDPOINT, {
-    method: "POST",
+    method: 'POST',
     headers: {
       Authorization: `Basic ${basic_authorization}`,
-      "Content-Type": "application/x-www-form-urlencoded",
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: new URLSearchParams({
-      grant_type: "refresh_token",
-      active_refresh_token,
+      grant_type: 'refresh_token',
+      refresh_token: active_refresh_token,
     }),
   });
 
   // Spotify does not always supply a new refresh token, get new access_token details and default
   // to old refresh_token if a new one was not provided
-  const { access_token, expires_at, refresh_token } = await res.json();
+  const { access_token, expires_in, refresh_token } = await res.json();
   return {
-    access_token,
-    expires_at,
+    access_token: access_token,
+    expires_at: Math.floor(Date.now() / 1000) + expires_in,
     refresh_token: refresh_token ?? active_refresh_token,
   };
 }
@@ -36,10 +34,10 @@ export async function get_access_token(active_refresh_token: string) {
 /** Get a users details using an access_token */
 export async function get_user_details(access_token: string) {
   const res = await fetch(`${SPOTIFY_API_ROOT}/me`, {
-    method: "GET",
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${access_token}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
   });
 
@@ -56,7 +54,7 @@ export async function get_artists(access_token: string, artist_list: string) {
     {
       headers: {
         Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     }
   );
@@ -68,19 +66,16 @@ export async function get_artists(access_token: string, artist_list: string) {
   return res.json();
 }
 
-export async function search_for_playlist(
-  access_token: string,
-  playlist_name: string
-) {
+export async function search_for_playlist(access_token: string, playlist_name: string) {
   const res = await fetch(
     `${SPOTIFY_API_ROOT}/search?${new URLSearchParams({
-      type: "playlist",
+      type: 'playlist',
       q: playlist_name,
     })}`,
     {
       headers: {
         Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     }
   );
@@ -93,29 +88,24 @@ export async function search_for_playlist(
   return res.json();
 }
 
-export async function get_playlist_tracks(
-  access_token: string,
-  playlist_id: string
-) {
-  let next: string | null = "";
-  let url = `${SPOTIFY_API_ROOT}/playlists/${playlist_id}/tracks?${new URLSearchParams(
-    {
-      limit: "50",
-    }
-  )}`;
+export async function get_playlist_tracks(access_token: string, playlist_id: string) {
+  let next: string | null = '';
+  let url = `${SPOTIFY_API_ROOT}/playlists/${playlist_id}/tracks?${new URLSearchParams({
+    limit: '50',
+  })}`;
 
   let all_tracks: PlaylistTrack[] = [];
 
   do {
-    if (next !== "") {
+    if (next !== '') {
       url = next;
     }
 
     const res = await fetch(url, {
-      method: "GET",
+      method: 'GET',
       headers: {
         Authorization: `Bearer ${access_token}`,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     });
 
