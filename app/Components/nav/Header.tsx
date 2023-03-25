f08a260b51833e2871b01387c2646630ac2d7c5d
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-03-25 15:09:12 -0400

    add mobile nav skeleton

--- app/Components/nav/Header.tsx
+++ app/Components/nav/Header.tsx
@@ -20,8 +20,8 @@ export default function Header({ links }: HeaderProps) {
       key={link.label}
       href={link.href}
       className={`${
-        active === link.href ? 'text-gray bg-green ' : 'text-white hover:text-gray-light'
-      } flex justify-center items-center h-full px-4 text-2xl font-bold`}
+        active === link.href ? 'sm:text-gray sm:bg-green ' : 'text-white hover:text-gray-light'
+      } flex justify-center items-center h-full sm:px-4 text-2xl font-bold`}
       onClick={(event) => {
         setActive(link.href);
       }}
@@ -31,17 +31,43 @@ export default function Header({ links }: HeaderProps) {
   ));
 
   return (
-    <header className="flex justify-between items-center w-full h-16 mb-32 border-b-4 border-green">
-      <div className="flex justify-start items-center h-full">{items}</div>
-      {session && (
-        <button
-          className="flex justify-center items-center h-full px-4 text-2xl font-bold text-white hover:text-gray-light"
-          name="Log Out"
-          onClick={() => signOut()}
-        >
-          log out
-        </button>
-      )}
+    <header className="sticky top-0 flex justify-between items-center w-full h-16 mb-32 border-b-4 border-green bg-gray-dark">
+      <nav className="hidden sm:flex justify-start items-center h-full">
+        {items}
+        {session && (
+          <button
+            className="flex justify-center items-center h-full px-4 text-2xl font-bold text-white hover:text-gray-light"
+            name="Log Out"
+            onClick={() => signOut()}
+          >
+            log out
+          </button>
+        )}
+      </nav>
+      {/* Mobile Nav */}
+      <nav className="sm:hidden w-full h-full">
+        <ul className="flex justify-between items-center h-full w-full px-4 sm:hidden">
+          <li className="h-full">{items[0]}</li>
+          <li className="flex items-center h-full">
+            <button className="h-full">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
+            </button>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
