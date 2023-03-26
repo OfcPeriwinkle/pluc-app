e35b0040e6aa23f962fa5cc5c5de8663d6ae5eac
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-03-26 13:51:20 -0400

    restyle search results to better match Spotify

--- app/Components/search/PlaylistCard.tsx
+++ app/Components/search/PlaylistCard.tsx
@@ -30,29 +30,19 @@ export default function PlaylistCard({
   }
 
   return (
-    <div className="group flex min-w-fit max-w-fit flex-col items-center justify-center gap-6 rounded-xl bg-gray-light bg-opacity-10 p-6 shadow-xl duration-150 ease-in-out hover:bg-opacity-20">
-      <div className="flex w-full flex-row items-center justify-evenly gap-6">
+    <div>
+      <button className="flex flex-col justify-center items-center p-4 bg-gray rounded-lg w-52">
         <Image
           src={playlist_image_url}
           width={160}
           height={160}
           alt="Playlist Image"
-          className="bightness-75 h-40 w-40 rounded-xl object-cover group-hover:shadow-xl group-hover:brightness-100 "
+          className="h-44 w-44 object-contain" // object-contain is needed for Spotify approval
         />
-        <div>
-          <h1 className="max-w-md truncate text-ellipsis text-3xl font-semibold text-white">
-            {playlist_name}
-          </h1>
-          <h3 className="max-w-md truncate text-ellipsis text-xl font-medium text-gray-light">
-            by {user_display_name}
-          </h3>
-        </div>
-      </div>
-      <button
-        className="rounded-full bg-green p-4 font-medium text-white duration-150 hover:scale-105 hover:shadow-xl"
-        onClick={handle_click}
-      >
-        Find Duplicates
+        <section className="mt-2 w-full text-start">
+          <h3 className="truncate w-full font-semibold">{playlist_name}</h3>
+          <p className="truncate w-full text-gray-light">{user_display_name}</p>
+        </section>
       </button>
     </div>
   );
--- app/Components/search/PlaylistSearchResults.tsx
+++ app/Components/search/PlaylistSearchResults.tsx
@@ -1,24 +1,22 @@
-import PlaylistCard from "./PlaylistCard";
-import type { SimplifiedPlaylist } from "spotify-types/typings/playlist";
+import PlaylistCard from './PlaylistCard';
+import type { SimplifiedPlaylist } from 'spotify-types/typings/playlist';
 
 export interface PlaylistSearchResultsProps {
   playlists: SimplifiedPlaylist[];
 }
 
-export default function PlaylistSearchResults({
-  playlists,
-}: PlaylistSearchResultsProps) {
+export default function PlaylistSearchResults({ playlists }: PlaylistSearchResultsProps) {
   const processed_playlists = playlists.map((playlist) => {
     return {
       playlist_id: playlist.id,
       playlist_image_url: playlist.images[0].url,
       playlist_name: playlist.name,
-      display_name: playlist.owner.display_name ?? "No Name",
+      display_name: playlist.owner.display_name ?? 'No Name',
     };
   });
 
   return (
-    <div className="flex flex-wrap justify-center items-center gap-10 p-14">
+    <div className="flex flex-wrap justify-center items-center gap-10 mt-6 sm:mt-10">
       {processed_playlists.map((playlist_details, index) => {
         return (
           <PlaylistCard
