efb472b385b6eb28a5b2c5f85bbadcbeb87aeaa9
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-03-28 20:19:40 -0400

    add headlessui; use Dialog in RemoveModal

--- app/Components/overlays/RemoveModal.tsx
+++ app/Components/overlays/RemoveModal.tsx
@@ -1,26 +1,43 @@
 'use client';
 
 import { useState } from 'react';
+import { Dialog } from '@headlessui/react';
 
 export default function RemoveModal() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   return (
-    <aside className="rounded-md bg-gray bg-opacity-30 p-4">
-      <div>
-        <h2 className="text-xl font-semibold">Remove track?</h2>
-        <p className="mt-2 text-lg text-gray-light">
-          Are you sure you want to remove this track from your playlist?
-        </p>
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      className="relative z-50"
+    >
+      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div className="fixed inset-0 flex items-center justify-center p-4">
+        <Dialog.Panel className="mx-auto max-w-sm rounded-md bg-gray p-4">
+          <Dialog.Title className="text-xl font-semibold">
+            Remove track?
+          </Dialog.Title>
+          <Dialog.Description className="mt-2 text-lg text-gray-light">
+            This will permanently remove the track from your playlist.
+          </Dialog.Description>
+
+          <div className="mt-4 flex flex-col justify-around gap-4 sm:flex-row">
+            <button
+              className="h-11 rounded-full bg-red-600 font-semibold duration-200 ease-in-out hover:scale-105 sm:h-12 sm:w-40"
+              onClick={() => setOpen(false)}
+            >
+              Remove
+            </button>
+            <button
+              className="h-11 rounded-full border-2 border-white font-semibold duration-200 ease-in-out hover:scale-105 hover:bg-gray-light hover:bg-opacity-25 sm:h-12 sm:w-40"
+              onClick={() => setOpen(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </Dialog.Panel>
       </div>
-      <div className="mt-4 flex flex-col-reverse justify-around gap-4 sm:flex-row">
-        <button className="h-11 rounded-full border-2 border-white duration-200 ease-in-out hover:scale-105 hover:bg-gray-light hover:bg-opacity-25 focus:scale-95 sm:h-12 sm:w-40">
-          Cancel
-        </button>
-        <button className="h-11 rounded-full bg-red-600 duration-200 ease-in-out hover:scale-105 focus:scale-95 sm:h-12 sm:w-40">
-          Remove
-        </button>
-      </div>
-    </aside>
+    </Dialog>
   );
 }
