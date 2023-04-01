9309d0befb974b66281297fc1d3afef6a9b0235b
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-04-01 14:55:19 -0400

    add state management for thumbs up/down; make textarea initial focus

--- app/Components/overlays/FeedbackModal.tsx
+++ app/Components/overlays/FeedbackModal.tsx
@@ -1,20 +1,29 @@
 'use client';
 
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import {
   HandThumbDownIcon,
   HandThumbUpIcon,
 } from '@heroicons/react/24/outline';
 
-export default function RemoveModal() {
+enum Thumbs {
+  NONE,
+  UP,
+  DOWN,
+}
+
+export default function FeedbackModal() {
   const [open, setOpen] = useState(true);
+  const [thumbs, setThumbs] = useState(Thumbs.NONE);
+  const textarea_ref = useRef(null);
 
   return (
     <Dialog
+      className="relative z-50"
+      initialFocus={textarea_ref}
       open={open}
       onClose={() => setOpen(false)}
-      className="relative z-50"
     >
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
       <div className="fixed inset-0 flex items-center justify-center p-4">
@@ -29,11 +38,25 @@ export default function RemoveModal() {
 
           <form className="mt-4">
             <div className="flex w-full items-center justify-center gap-4">
-              <button className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-white transition duration-200 ease-in-out hover:bg-opacity-50">
+              <button
+                className={`flex h-14 w-14 items-center justify-center rounded-full border-2 border-white transition duration-200 ease-in-out hover:scale-105 ${
+                  thumbs == Thumbs.DOWN ? 'bg-red-600' : ''
+                }`}
+                onClick={() => setThumbs(Thumbs.DOWN)}
+                name="thumbs-down"
+                type="button"
+              >
                 <HandThumbDownIcon className="h-10 w-10" />
               </button>
 
-              <button className="flex h-14 w-14 items-center justify-center rounded-full border-2 border-white transition duration-200 ease-in-out hover:bg-opacity-50">
+              <button
+                className={`flex h-14 w-14 items-center justify-center rounded-full border-2 border-white transition duration-200 ease-in-out hover:scale-105 ${
+                  thumbs == Thumbs.UP ? 'bg-green hover:bg-green' : ''
+                }`}
+                onClick={() => setThumbs(Thumbs.UP)}
+                name="thumbs-up"
+                type="button"
+              >
                 <HandThumbUpIcon className="h-10 w-10" />
               </button>
             </div>
@@ -41,21 +64,24 @@ export default function RemoveModal() {
             <textarea
               className="mt-4 block h-44 w-full resize-none rounded-md border-white bg-gray-dark p-2 text-white"
               placeholder="Enter optional feedback message..."
+              ref={textarea_ref}
             />
             <p className="mt-2 text-gray-light">0 / 200</p>
 
             <div className="mt-4 flex flex-col justify-around gap-4 sm:flex-row">
               <button
                 className="h-11 rounded-full bg-green font-semibold duration-200 ease-in-out hover:scale-105 sm:h-12 sm:w-40"
-                name="submit"
                 onClick={() => setOpen(false)}
+                name="submit"
+                type="button"
               >
                 Send
               </button>
               <button
                 className="h-11 rounded-full border-2 border-white font-semibold duration-200 ease-in-out hover:scale-105 hover:bg-gray-light hover:bg-opacity-25 sm:h-12 sm:w-40"
-                name="cancel"
                 onClick={() => setOpen(false)}
+                name="cancel"
+                type="button"
               >
                 Cancel
               </button>
