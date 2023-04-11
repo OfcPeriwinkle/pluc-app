ccce44576101a18edb2979c3fab387394af89fea
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-04-11 19:00:28 -0400

    add remove_tracks to handle spotify api track removal endpoint

--- pages/api/remove_track.ts
+++ pages/api/remove_track.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import { remove_tracks } from '../../lib/spotify';
 
 export default async function remove_track(
   req: NextApiRequest,
@@ -7,7 +8,7 @@ export default async function remove_track(
 ) {
   const session = await getSession({ req });
 
-  if (!session) {
+  if (!session || !session.access_token) {
     res.status(401).json({ error: 'Invalid session' });
     return;
   }
@@ -15,9 +16,20 @@ export default async function remove_track(
   const { track_id, playlist_id } = req.body;
 
   if (!track_id || !playlist_id) {
-    res.status(400).json({ error: 'Missing track_id and/or playlist_id' });
-    return;
+    return res
+      .status(400)
+      .json({ error: 'Missing track_id and/or playlist_id' });
   }
 
-  res.status(200).json({ success: true });
+  try {
+    const snapshot_id = await remove_tracks(session.access_token, playlist_id, [
+      track_id,
+    ]);
+  } catch (err) {
+    return res.status(500).json({
+      error: `Failed to remove track from playlist`,
+    });
+  }
+
+  return res.status(200).json({ success: true });
 }
