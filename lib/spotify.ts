b1cbc8673b92ca5c7904994a2b2e95b04c058282
OfcPeriwinkle <ajwald@ucdavis.edu>
2023-11-09 19:03:40 -0800

    camelCase library functions

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -4,23 +4,23 @@ import { PlaylistTrack } from 'spotify-types';
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 const SPOTIFY_API_ROOT = 'https://api.spotify.com/v1';
 
-const client_id = process.env.SPOTIFY_ID;
-const client_secret = process.env.SPOTIFY_SECRET;
-const basic_authorization = Buffer.from(
-  `${client_id}:${client_secret}`
-).toString('base64');
+const clientID = process.env.SPOTIFY_ID;
+const clientSecret = process.env.SPOTIFY_SECRET;
+const basicAuthorization = Buffer.from(`${clientID}:${clientSecret}`).toString(
+  'base64'
+);
 
 /** Use a refresh_token to acquire a new access_token */
-export async function get_access_token(active_refresh_token: string) {
+export async function getAccessToken(activeRefreshToken: string) {
   const res = await fetch(TOKEN_ENDPOINT, {
     method: 'POST',
     headers: {
-      Authorization: `Basic ${basic_authorization}`,
+      Authorization: `Basic ${basicAuthorization}`,
       'Content-Type': 'application/x-www-form-urlencoded',
     },
     body: new URLSearchParams({
       grant_type: 'refresh_token',
-      refresh_token: active_refresh_token,
+      refresh_token: activeRefreshToken,
     }),
   });
 
@@ -30,16 +30,16 @@ export async function get_access_token(active_refresh_token: string) {
   return {
     access_token: access_token,
     expires_at: Math.floor(Date.now() / 1000) + expires_in,
-    refresh_token: refresh_token ?? active_refresh_token,
+    refresh_token: refresh_token ?? activeRefreshToken,
   };
 }
 
 /** Get a users details using an access_token */
-export async function get_user_details(access_token: string) {
+export async function getUserDetails(accessToken: string) {
   const res = await fetch(`${SPOTIFY_API_ROOT}/me`, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${access_token}`,
+      Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json',
     },
   });
@@ -51,19 +51,19 @@ export async function get_user_details(access_token: string) {
   return res.json();
 }
 
-export async function get_artists(access_token: string, artist_ids: string[]) {
+export async function getArtists(accessToken: string, artistIDs: string[]) {
   // Send request for every 50 artists
-  const id_batches = chunk(artist_ids, 50);
+  const IDBatches = chunk(artistIDs, 50);
 
   const res = await Promise.all(
-    id_batches.map(async (batch) => {
+    IDBatches.map(async (batch) => {
       const res = await fetch(
         `${SPOTIFY_API_ROOT}/artists?${new URLSearchParams({
           ids: batch.join(','),
         })}`,
         {
           headers: {
-            Authorization: `Bearer ${access_token}`,
+            Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json',
           },
         }
@@ -82,18 +82,18 @@ export async function get_artists(access_token: string, artist_ids: string[]) {
   return [].concat.apply([], res);
 }
 
-export async function search_for_playlist(
-  access_token: string,
-  playlist_name: string
+export async function searchForPlaylist(
+  accessToken: string,
+  playlistName: string
 ) {
   const res = await fetch(
     `${SPOTIFY_API_ROOT}/search?${new URLSearchParams({
       type: 'playlist',
-      q: playlist_name,
+      q: playlistName,
     })}`,
     {
       headers: {
-        Authorization: `Bearer ${access_token}`,
+        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       },
     }
@@ -107,18 +107,18 @@ export async function search_for_playlist(
   return res.json();
 }
 
-export async function get_playlist_tracks(
-  access_token: string,
-  playlist_id: string
+export async function getPlaylistTracks(
+  accessToken: string,
+  playlistID: string
 ) {
   let next: string | null = '';
-  let url = `${SPOTIFY_API_ROOT}/playlists/${playlist_id}/tracks?${new URLSearchParams(
+  let url = `${SPOTIFY_API_ROOT}/playlists/${playlistID}/tracks?${new URLSearchParams(
     {
       limit: '50',
     }
   )}`;
 
-  let all_tracks: PlaylistTrack[] = [];
+  let allTracks: PlaylistTrack[] = [];
 
   do {
     if (next !== '') {
@@ -128,7 +128,7 @@ export async function get_playlist_tracks(
     const res = await fetch(url, {
       method: 'GET',
       headers: {
-        Authorization: `Bearer ${access_token}`,
+        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       },
     });
@@ -138,37 +138,37 @@ export async function get_playlist_tracks(
     }
 
     // Process response from Spotify
-    const spotify_res = await res.json();
-    all_tracks = [...all_tracks, ...(spotify_res.items as PlaylistTrack[])];
+    const spotifyRes = await res.json();
+    allTracks = [...allTracks, ...(spotifyRes.items as PlaylistTrack[])];
 
     // Get url to next group of results, or null if all tracks have been collected
-    next = spotify_res.next as string | null;
+    next = spotifyRes.next as string | null;
   } while (next !== null);
 
   return new Promise<PlaylistTrack[]>((resolve) => {
-    resolve(all_tracks);
+    resolve(allTracks);
   });
 }
 
-export async function remove_tracks(
-  access_token: string,
-  playlist_id: string,
-  track_ids: string[]
+export async function removeTracks(
+  accessToken: string,
+  playlistID: string,
+  trackIDs: string[]
 ) {
-  if (track_ids.length >= 100) {
+  if (trackIDs.length >= 100) {
     throw new Error('Cannot remove more than 100 tracks at a time');
   }
 
   const res = await fetch(
-    `${SPOTIFY_API_ROOT}/playlists/${playlist_id}/tracks`,
+    `${SPOTIFY_API_ROOT}/playlists/${playlistID}/tracks`,
     {
       method: 'DELETE',
       headers: {
-        Authorization: `Bearer ${access_token}`,
+        Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        tracks: track_ids.map((id) => ({ uri: `spotify:track:${id}` })),
+        tracks: trackIDs.map((id) => ({ uri: `spotify:track:${id}` })),
       }),
     }
   );
