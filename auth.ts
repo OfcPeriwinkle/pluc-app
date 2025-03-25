f326bc9186983f8c69d206fb9d96a4039a2eb195
OfcPeriwinkle <ajwald98@gmail.com>
2025-03-24 19:06:43 -0700

    add scopes to auth url

--- auth.ts
+++ auth.ts
@@ -1,6 +1,12 @@
 import NextAuth from "next-auth";
 import Spotify from "next-auth/providers/spotify";
 
+const scopes: string[] = ["user-read-email", "user-library-read"];
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  providers: [Spotify],
+  providers: [
+    Spotify({
+      authorization: `https://accounts.spotify.com/authorize?scope=${scopes.join(",")}`,
+    }),
+  ],
 });
